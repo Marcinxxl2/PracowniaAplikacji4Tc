be37083af5b89ea790ed3b0ef64d9be3a63afd14
Marcinxxl2 <marcinxxl22@gmail.com>
2017-09-13 15:42:23 +0200

    zadanie trzeba dokończyć

---
+++ 13.09.2017/js/funkcje.js
@@ -0,0 +1,52 @@
+
+
+/*
+function witaj(imie) {
+    document.write('Witaj ' + imie);
+
+}
+
+var name = 'Maciej'
+witaj(name);
+
+var a = prompt('Podaj a:');
+var b = prompt('Podaj b:');
+
+function obliczPoleProstokata(a, b) {
+    var pole = a * b;
+    return pole;
+}
+
+console.log(obliczPoleProstokata(a, b));
+*/
+
+/*
+function poleObjetosc(szerokosc, dlugosc, wysokosc) {
+    var pole = szerokosc * dlugosc;
+    var objetosc = pole * wysokosc;
+    var wynik = [pole, objetosc];
+    return wynik;
+}
+*/
+
+//console.log(poleObjetosc(2, 3, 4));
+
+/*var pole = poleObjetosc(2, 3, 4)[0];
+var objetosc = poleObjetosc(2, 3, 4)[1];*/
+
+/*console.log('Pole: ' + pole);
+console.log('Pole: ' + objetosc);*/
+
+/*
+var wybor = prompt('Podaj wartość: ', '0 - pole, 1 - objętość');
+
+console.log(poleObjetosc(2, 3, 4)[wybor]);
+*/
+
+
+//Napisać skrypt, który oblicza obwód oraz objętość stożka. Dane użytkownik podaje z klawiatury i zaznacza co chce obliczyć w radio. Wynik wypisuje się w divie
+
+function oblicz() {
+    var wybor = document.forms
+}
+
