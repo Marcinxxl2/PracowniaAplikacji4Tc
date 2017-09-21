df541537f18188f914b23376cb299dd2784449f4
Marcinxxl2 <marcinxxl22@gmail.com>
2017-09-21 10:13:05 +0200

    dostep

--- egzaminy/egzamin E.14-01-15.01/js/egzamin2.js
+++ egzaminy/egzamin E.14-01-15.01/js/egzamin2.js
@@ -1,9 +1,11 @@
 document.getElementById("potegowanie").onclick = function () {
     var liczba1 = parseFloat(document.getElementById("pierwsza").value);
     var liczba2 = parseFloat(document.getElementById("druga").value);
-    
+    var elWynik = document.getElementById("wynik");
     if (isNaN(liczba1) || isNaN(liczba2))
-        document.getElementById("wynik").textContent = "Wpisz wymagane dane";
+        elWynik.textContent = "Wpisz wymagane dane";
+    else if (liczba2 < 0)
+        elWynik.textContent = "Wykładnik nie może być ujemny"
     else
-        document.getElementById("wynik").textContent = Math.pow(liczba1, liczba2);
+        elWynik.textContent = "Wynik działania wynosi: " + Math.pow(liczba1, liczba2);
 }
