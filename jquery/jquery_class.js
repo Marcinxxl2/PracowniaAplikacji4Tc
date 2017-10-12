0e528ae5f479b9dc1015b2e6261613cb04151744
Marcinxxl2 <marcinxxl22@gmail.com>
2017-10-12 11:13:41 +0200

    www

--- jquery/jquery_class.js
+++ jquery/jquery_class.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+	$('#tlo1').click(function(){
+		$('#akapit').addClass('tlo1');
+	});
+
+	$('#tlo2').click(function(){
+		$('#akapit').toggleClass('tlo2');
+	});
+
+	$('#tlo3').click(function(){
+		//$('#akapit').removeClass('tlo1');
+		//$('#akapit').removeClass();
+		alert($('#akapit').hasClass('tlo1'));
+	})
 
 
 });
